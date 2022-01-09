fb344a338dbba7e5e764071fec21d1b99c35572a
Talar1997 <adam.talarczyk@hotmail.com>
2022-01-09 23:02:31 +0100

    Added station markers on map (basic implementation)

--- app/src/main/java/com/example/airqualityandroid/ui/map/MapsFragment.kt
+++ app/src/main/java/com/example/airqualityandroid/ui/map/MapsFragment.kt
@@ -6,17 +6,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.airquality.api.ApiClient
+import com.example.airquality.data.station.Station
 import com.example.airqualityandroid.R
 
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import retrofit2.Call
+import retrofit2.Callback
 
 class MapsFragment : Fragment() {
-
+    private val defaultLocation = LatLng(51.9194, 19.1451)
     private val callback = OnMapReadyCallback { googleMap ->
         /**
          * Manipulates the map once available.
@@ -27,9 +30,31 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        //TODO: get users LatLng
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, 7.0F))
+
+        //TODO: move it to ViewModel file
+        val stationCall: Call<List<Station>> = ApiClient().getStationService().getAllStations()
+        stationCall.enqueue(object : Callback<List<Station>> {
+            override fun onFailure(call: Call<List<Station>>?, t: Throwable?) {
+                println("fail")
+            }
+
+            override fun onResponse(
+                call: Call<List<Station>>,
+                response: retrofit2.Response<List<Station>>
+            ) {
+                val list: List<Station> = response.body()!!
+                list.forEach { station ->
+
+                    //TODO: add other caption and make somehow on click to open other activity with details
+                    val latLng = LatLng(station.gegrLat.toDouble(),station.gegrLon.toDouble())
+                    val caption: String = station.city.name
+                    googleMap.addMarker(MarkerOptions().position(latLng).title(caption))
+                }
+            }
+        })
     }
 
     override fun onCreateView(
@@ -45,4 +70,5 @@ class MapsFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
+
 }
--- app/src/main/java/com/example/airqualityandroid/ui/stations/StationsAdapter.kt
+++ app/src/main/java/com/example/airqualityandroid/ui/stations/StationsAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.airqualityandroid.ui.stationsDeprecated
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -37,6 +38,17 @@ class StationsAdapter(): RecyclerView.Adapter<StationsAdapter.ViewHolder>() {
         val stationViewModel = stations[position]
         holder.stationLocation.text = stationViewModel.stationName
         holder.stationCondition.text = stationViewModel.addressStreet
+
+        //TODO: when onClick on station, move to other context with detailed data for station
+        // example:
+        /*holder.itemView.setOnClickListener{
+            val intent = Intent(context, StationActivity::class.java)   //Create activity for station details
+            intent.putExtra("StationId", list[position].id)             //list here equals for stations
+            intent.putExtra("StationName", list[position].stationName)
+            intent.putExtra("StationAddress", list[position].addressStreet)
+            context.startActivity(intent)
+        }*/
+
         holder.itemView.setOnClickListener{
             Toast.makeText(context, stationViewModel.stationName, Toast.LENGTH_SHORT).show()
         }
